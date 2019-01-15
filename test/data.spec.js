e7fa36664e2fde4dc8e381624f4bada166db6c9a
“CatalinaCampos” <catalina.camposcaceres9@gmail.com>
2019-01-15 15:17:39 -0300

    TDD de función filtrar

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,18 @@
+global.window = global;
+global.chai = require('chai');
+global.expect = require("chai").expect;
+global.assert = require("chai").assert;
+require('../src/data/lol/lol.js')
 require('../src/data.js');
 
+describe('window.LOL', () => {
+  it('debería ser un objeto', () => {
+    window.assert.equal(typeof window.LOL, 'object' );
+  })
+})
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
+describe('window.LOL.filterData', () => {
+  it('debería devolver "Aarox" al filtrar "Fighter"', () => {
+    window.assert.equal(window.data.filterData(Object.values(window.LOL.data), "Fighter")[0].name, "Aatrox")
+  })
+})
