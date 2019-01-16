00d2a1b0ef1fd1ab109b2df0b5f729299c6f0594
“CatalinaCampos” <catalina.camposcaceres9@gmail.com>
2019-01-16 09:41:15 -0300

    Función mostrar cards con información inicial (splash, name, title)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 // aquí va manejo del dom
 const dataLOL = Object.values(window.LOL.data);
+
 function allData(){
     let result = [];
-    for (let i = 0; i < data.length; i++){
-      result.push(data[0].name + " " + data[0].title + " " + data[0].tags[0] + ", " + data[0].tags[1]);
+    for (let i = 0; i < dataLOL.length; i++){
+      result.push(dataLOL[i].name + " " + dataLOL[i].title);
     }
     return result;
   }
+function lolCards(){  
+    let all = [];
+    for (let i = 0; i < dataLOL.length; i++){
+        all.push(`<div class="container>
+        <div class="card" style="width: 18rem;">
+        <img src="${dataLOL[i].splash}" class="card-img-top" alt="${dataLOL[i].id}">
+        <div class="card-body">
+        <h5 class="card-title">${dataLOL[i].name}</h5>
+        <h6 class="card-subtitle">${dataLOL[i].title}</h6>
+        </div>
+        </div>
+        </div>`);
+    }
+    return all;
+}
+document.getElementById("root").innerHTML = lolCards();
+
 
 let tagsTypes = ["Fighter", "Tank", "Mage", "Assassin", "Support", "Marksman"];
+     
